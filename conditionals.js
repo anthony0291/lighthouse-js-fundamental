d519de737e0d77d2b33a1b5ff046293721427c74
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-05-03 22:00:46 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,6 @@
-const temp = -2;
+const isCitizen = true;
+const age = 26;
 
-if (temp < 0) {
-  console.log("Make sure to pick out a scarf!")
-
-} else if (temp < 15) {
-  console.log("Short sleeves won't do!");
-
-} else {
-  console.log("Short sleeves are fine");
+if (isCitizen && age > 18) {
+  console.log("Youre are eligible to vote.");
 }
-
-console.log("You're ready to go!");
